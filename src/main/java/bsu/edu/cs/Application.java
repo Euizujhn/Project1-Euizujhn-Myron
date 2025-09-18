cd03cee3a1685b75047009fb950c53c7349d27ce
Euizujhn <euizujhn11@gmail.com>
2025-09-18 15:20:54 -0400

    Update Application.java

--- src/main/java/bsu/edu/cs/Application.java
+++ src/main/java/bsu/edu/cs/Application.java
@@ -1,8 +1,12 @@
 package bsu.edu.cs;
 
 public class Application {
-    //Take Wiki Page
-
+    public Application() throws IOException {
+        URL url = new URL("https://en.wikipedia.org/wiki/Video_game");
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent","Revision Reporter/0.1 (ej.wade@bsu.edu)");
+        InputStream inputStream = connection.getInputStream();
+    }
     //See User Changes
 
     //See Location Changes
