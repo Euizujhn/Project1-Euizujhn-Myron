00fd4d475b75bdc63d4715efc6d347978cdef64a
Euizujhn <euizujhn11@gmail.com>
2025-09-18 13:03:49 -0400

    Project

---
+++ src/main/java/bsu/edu/cs/Application.java
@@ -0,0 +1,9 @@
+package bsu.edu.cs;
+
+public class Application {
+    //Take Wiki Page
+
+    //See User Changes
+
+    //See Location Changes
+}
---
+++ src/main/java/bsu/edu/cs/Journalist.java
@@ -0,0 +1,8 @@
+package bsu.edu.cs;
+
+public class Journalist {
+    public static void main(String[] args) {
+        //Journalist -> Pick Wiki Page -> See Changes
+
+    }
+}
