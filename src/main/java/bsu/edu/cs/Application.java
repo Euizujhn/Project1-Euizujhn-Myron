411e1e1c1f98b10dcc30646e052dc4912117bbe9
Euizujhn <euizujhn11@gmail.com>
2025-09-24 23:54:01 -0400

    Update Application.java

--- src/main/java/bsu/edu/cs/Application.java
+++ src/main/java/bsu/edu/cs/Application.java
@@ -1,13 +1,40 @@
 package bsu.edu.cs;
 
-public class Application {
-    public Application() throws IOException {
-        URL url = new URL("https://en.wikipedia.org/wiki/Video_game");
-        URLConnection connection = url.openConnection();
-        connection.setRequestProperty("User-Agent","Revision Reporter/0.1 (ej.wade@bsu.edu)");
-        InputStream inputStream = connection.getInputStream();
-    }
-    //See User Changes
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.util.Scanner;
 
-    //See Location Changes
-}
+
+public class Application {
+// The Actual Menu, Journalist Picks the Info from this
+
+//Go to Article, Basic Info about the Wiki, The Most Recent Wiki Changes
+
+//Display info from the Wiki_Reader
+
+
+
+    static Scanner input = new Scanner(System.in);
+ Wikipedia_Reader reader = new Wikipedia_Reader();
+ String title = getTheTitle();
+
+    public static void main(String[] args) throws IOException {
+        String title = getTheTitle();
+
+        if(title == null || title.isBlank()){
+            System.err.println("Error: Page not Found");
+            return;
+        }
+    }
+
+        public static String getTheTitle(){
+            System.out.println("Enter an Article from the list:\nVideo Game\n");
+            String Wiki = input.nextLine();
+            return Wiki;
+        }
+    }
+
+
+
+
